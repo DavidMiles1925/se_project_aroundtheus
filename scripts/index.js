d69dfa65fa846cc1ce989a68eb2d124939a82083
David Miles <davidmiles1925@gmail.com>
2022-12-04 13:04:04 -0600

    S4.4 feat: save profile information from modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Card array
 let initialCards = [
   {
     name: "Yosimite Valley",
@@ -25,17 +26,48 @@ let initialCards = [
   },
 ];
 
-const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".modal__close-button");
+// Select page elements
 const modalOverlay = document.querySelector(".modal");
+const formElement = document.querySelector(".form");
+const profileElement = document.querySelector(".profile");
 
+//Select buttons
+const closeButton = modalOverlay.querySelector(".modal__close-button");
+const submitButton = formElement.querySelector(".form__submit-button");
+const editButton = profileElement.querySelector(".profile__edit-button");
+
+// Select form and input text elements
+const formNameText = formElement.querySelector(".form__input_type_name");
+const formAboutText = formElement.querySelector(".form__input_type_about");
+const currentNameText = profileElement.querySelector(".profile__title");
+const currentAboutText = profileElement.querySelector(".profile__description");
+
+// Show modal window and fill in fields with current profile values
 function displayModal() {
+  // Update input field text
+  formNameText.value = currentNameText.textContent;
+  formAboutText.value = currentAboutText.textContent;
+  // Show modal
   modalOverlay.classList.add("modal_open");
 }
 
+// Hide modal window
 function hideModal() {
   modalOverlay.classList.remove("modal_open");
 }
 
+// Handle click of save button
+function handleFormSubmit(evt) {
+  //Prevent default browser behavior
+  evt.preventDefault();
+  // Update profile text
+  currentNameText.textContent = formNameText.value;
+  currentAboutText.textContent = formAboutText.value;
+  // Hide modal
+  hideModal();
+}
+
+//Event listeners
 editButton.addEventListener("click", displayModal);
 closeButton.addEventListener("click", hideModal);
+submitButton.addEventListener("click", handleFormSubmit);
