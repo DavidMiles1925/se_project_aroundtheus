37ffa56048dca148926134b994392e7d3cddde52
David Miles <davidmiles1925@gmail.com>
2022-12-03 22:35:42 -0600

    Sprint 4, Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,18 @@ let initialCards = [
     link: "../images/lago.jpg",
   },
 ];
+
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".modal__close-button");
+const modalOverlay = document.querySelector(".modal");
+
+function displayModal() {
+  modalOverlay.classList.add("modal_open");
+}
+
+function hideModal() {
+  modalOverlay.classList.remove("modal_open");
+}
+
+editButton.addEventListener("click", displayModal);
+closeButton.addEventListener("click", hideModal);
