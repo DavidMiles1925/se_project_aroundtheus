0173f02a1de98e109e8b171016f0cea3d6659d16
David Miles <davidmiles1925@gmail.com>
2022-12-04 13:43:24 -0600

    S4.4 refactor: cards displayed from array

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 let initialCards = [
   {
     name: "Yosimite Valley",
-    link: "../images/yosimite.jpg",
+    link: "../images/yosemite.jpg",
   },
   {
     name: "Lake Louise",
@@ -26,48 +26,57 @@ let initialCards = [
   },
 ];
 
-// Select page elements
 const modalOverlay = document.querySelector(".modal");
 const formElement = document.querySelector(".form");
 const profileElement = document.querySelector(".profile");
 
-//Select buttons
 const closeButton = modalOverlay.querySelector(".modal__close-button");
 const submitButton = formElement.querySelector(".form__submit-button");
 const editButton = profileElement.querySelector(".profile__edit-button");
 
-// Select form and input text elements
 const formNameText = formElement.querySelector(".form__input_type_name");
 const formAboutText = formElement.querySelector(".form__input_type_about");
 const currentNameText = profileElement.querySelector(".profile__title");
 const currentAboutText = profileElement.querySelector(".profile__description");
 
-// Show modal window and fill in fields with current profile values
 function displayModal() {
-  // Update input field text
   formNameText.value = currentNameText.textContent;
   formAboutText.value = currentAboutText.textContent;
-  // Show modal
+
   modalOverlay.classList.add("modal_open");
 }
 
-// Hide modal window
 function hideModal() {
   modalOverlay.classList.remove("modal_open");
 }
 
-// Handle click of save button
 function handleFormSubmit(evt) {
-  //Prevent default browser behavior
   evt.preventDefault();
-  // Update profile text
+
   currentNameText.textContent = formNameText.value;
   currentAboutText.textContent = formAboutText.value;
-  // Hide modal
+
   hideModal();
 }
 
-//Event listeners
+function getCardElement(cardName, cardLink) {
+  let cardTemplate = document.querySelector("#card").content;
+  let cardsDisplayed = document.querySelector(".cards");
+
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = cardName;
+  cardElement.querySelector(".card__image").src = cardLink;
+
+  cardsDisplayed.append(cardElement);
+}
+
+for (card of initialCards) {
+  cardName = card.name;
+  cardLink = card.link;
+  getCardElement(cardName, cardLink);
+}
+
 editButton.addEventListener("click", displayModal);
 closeButton.addEventListener("click", hideModal);
 submitButton.addEventListener("click", handleFormSubmit);
