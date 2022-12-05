32af85dd72aa61b044cf80f828c2bfcbb7fddd5b
David Miles <davidmiles1925@gmail.com>
2022-12-05 13:19:22 -0600

    S4.4 fix: submission 1 corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,33 @@
 let initialCards = [
   {
-    name: "Yosimite Valley",
+    name: "Yosemite Valley",
     link: "./images/yosemite.jpg",
+    alt: "yosemite",
   },
   {
     name: "Lake Louise",
     link: "./images/lake-louise.jpg",
+    alt: "lake louise",
   },
   {
     name: "Bald Mountains",
     link: "./images/bald-mountains.jpg",
+    alt: "bald mountains",
   },
   {
     name: "Latemar",
     link: "./images/latemar.jpg",
+    alt: "latemar",
   },
   {
     name: "Vanoise National Park",
     link: "./images/vanoise.jpg",
+    alt: "vanoise",
   },
   {
     name: "Lago di Braies",
     link: "./images/lago.jpg",
+    alt: "lago",
   },
 ];
 
@@ -29,7 +35,7 @@ const modalOverlay = document.querySelector(".modal");
 const formElement = document.querySelector(".form");
 const profileElement = document.querySelector(".profile");
 
-const closeButton = formElement.querySelector(".form__close-button");
+const closeButton = modalOverlay.querySelector(".modal__close-button");
 const submitButton = formElement.querySelector(".form__submit-button");
 const editButton = profileElement.querySelector(".profile__edit-button");
 
@@ -37,6 +43,9 @@ const formNameText = formElement.querySelector(".form__input_type_name");
 const formAboutText = formElement.querySelector(".form__input_type_about");
 const currentNameText = profileElement.querySelector(".profile__title");
 const currentAboutText = profileElement.querySelector(".profile__description");
+
+const cardTemplate = document.querySelector("#card").content;
+const cardsDisplayed = document.querySelector(".cards");
 
 function displayModal() {
   formNameText.value = currentNameText.textContent;
@@ -58,23 +67,31 @@ function handleFormSubmit(evt) {
   hideModal();
 }
 
-function getCardElement(cardName, cardLink) {
-  let cardTemplate = document.querySelector("#card").content;
-  let cardsDisplayed = document.querySelector(".cards");
-
+function getCardElement(data) {
   let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-  cardElement.querySelector(".card__title").textContent = cardName;
-  cardElement.querySelector(".card__image").src = cardLink;
+  cardElement.querySelector(".card__title").textContent = data["name"];
+  cardElement.querySelector(".card__image").src = data["link"];
+  cardElement.querySelector(".card__image").alt = data["alt"];
 
-  cardsDisplayed.append(cardElement);
+  return cardElement;
 }
 
-for (card of initialCards) {
-  cardName = card.name;
-  cardLink = card.link;
-  getCardElement(cardName, cardLink);
+function appendCards() {
+  for (card of initialCards) {
+    let data = ["name", "link", "alt"];
+
+    data["name"] = card.name;
+    data["link"] = card.link;
+    data["alt"] = card.alt;
+
+    let newCard = getCardElement(data);
+
+    cardsDisplayed.append(newCard);
+  }
 }
+
+appendCards();
 
 editButton.addEventListener("click", displayModal);
 closeButton.addEventListener("click", hideModal);
