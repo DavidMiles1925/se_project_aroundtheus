cbb71396431627a3dca4fb4ca6c55de2acea00af
David Miles <davidmiles1925@gmail.com>
2022-12-04 19:30:40 -0600

    S4.4 fix: image paths

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 let initialCards = [
   {
     name: "Yosimite Valley",
-    link: "../images/yosemite.jpg",
+    link: "./images/yosemite.jpg",
   },
   {
     name: "Lake Louise",
-    link: "../images/lake-louise.jpg",
+    link: "./images/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: "../images/bald-mountains.jpg",
+    link: "./images/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "../images/latemar.jpg",
+    link: "./images/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: "../images/vanoise.jpg",
+    link: "./images/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "../images/lago.jpg",
+    link: "./images/lago.jpg",
   },
 ];
 
